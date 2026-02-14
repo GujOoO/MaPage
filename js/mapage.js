851d488fe6d593e34fffc62bee02ac63ef31cb38
GujOoO <84066601+GujOoO@users.noreply.github.com>
2026-02-14 22:27:39 +0100

    upgrade

--- js/mapage.js
+++ js/mapage.js
@@ -4,7 +4,8 @@ const defaultZoom = 13;
 
 const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
-  attribution: '© OpenStreetMap contributors',
+  attribution: 'Map data from OpenStreetMap',
+  link: 'https://www.openstreetmap.org/copyright',
 });
 
 const map = L.map('map', {
